f6585e941562c30435e956f577978db35c170d40
Isak Horvath <48546805+tavro@users.noreply.github.com>
2021-11-05 12:05:30 +0100

    NEW: Add react-icons, Fix styling

--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -1,8 +1,13 @@
 import './topbar.css';
+import { FiMenu, FiUser } from 'react-icons/fi'
 
 function TopBar() {
     return <>
-        <div id="topBar">Räkning till d-sektionen</div>
+        <div id="topBar">
+            <FiMenu className="left"/>
+            <span>Räkning till d-sektionen</span>
+            <FiUser className="Right"/>
+        </div>
     </>
 }
 
