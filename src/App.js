b666b723e4c35c912d5f7836d16f633e14e70d18
Michelle Krejci <mickr592@student.liu.se>
2021-12-02 17:40:41 +0100

    Fixed router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import './App.css';
-import { MemoryRouter, Route, Switch } from "react-router-dom";
-import TopBar from './components/topbar/topbar.jsx';
-import MainPage from './components/mainpage/mainpage.jsx';
-import OverView from './components/overview/overview.jsx';
+import React from "react";
+import "./App.css";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import TopBar from "./components/topbar/topbar.jsx";
+import MainPage from "./components/mainpage/mainpage.jsx";
+import OverView from "./components/overview/overview.jsx";
 
 function App() {
-	return (
-		<div className='App'>
-			<MemoryRouter>
-				<TopBar/>
-				<Switch>
-					<Route exact path="/overview">
-						<OverView/>
-					</Route>
-					<Route exact path="/">
-						<MainPage/> :
-						<OverView/>
-					</Route>
-				</Switch>
-			</MemoryRouter>
-		</div>
-	);
+  return (
+    <div className="App">
+      <MemoryRouter>
+        <TopBar />
+        <Routes>
+          <Route exact path="/overview" element={<OverView />} />
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </MemoryRouter>
+    </div>
+  );
 }
 
 export default App;
